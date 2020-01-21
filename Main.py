bb6c25e6917ab12ac62574c728e99aa77ccad15a
Connor <31339874+ConniTheKiwi@users.noreply.github.com>
2020-01-21 13:58:39 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+false = False
+true = True
 debug = false
 
 #Offset values
@@ -43,13 +45,22 @@ running = True
 menuActive = 0
 subMenuActive = 0;
 
+def menu(Title, Body, Date):
+   print(Title + Body + Date)
+   return "w";
+
 while running:
 
 
     MainMenu = ['Cards', 'test']
     menu = [MainMenu]
 
-    card1 = ["Card 1 Menu", "Body"]
+    card1Option1 = ["Title", "body", "date"]
+    card1Option2 = ["Title2", "body2", "date2"]
+
+    card1Loc = ["O", card1Option1, "O", card1Option2]#menu(card1Option1[0], card1Option1[1], card1Option1[2])]
+    card1 = ["Card 1 Menu", "Body", card1Loc]#, card1Loc[0]]
+    
     card2 = ["Card 2 Menu", "Body"]
     cardsSubMenu = [card1, card2]
 
@@ -58,7 +69,7 @@ while running:
 
     displayMenu(menu)
     print(ScaleTextSides("", ' ', 20, True))
-    if(menuListActive[ int(menuActive) ][int(subMenuActive)] not str):
+    if(menuListActive[ int(menuActive) ][int(subMenuActive)]):
        for e in range(0, int( len(menuListActive[ int(menuActive) ][int(subMenuActive)] ))    ):
           # print("Test1: i: " + str(e))
            if(menuActive != 0):
@@ -68,12 +79,15 @@ while running:
                    print(ScaleTextSides(str(e+1)+ ": " + ((menuListActive[int(menuActive)])[int(subMenuActive)] )[e], ' ', 20, True))
            else:
                print(ScaleTextSides(str(e+1)+ ": " + str((menuListActive[int(menuActive)][int(subMenuActive)])[e]), ' ', 20, True))
-           
+
+    postModsSub = subMenuActive    
     if(menuActive == 0):
         menuActive = int(input(": "))
     elif(subMenuActive == 0):
         subMenuActive = int(input(": "))#int(input(menuListActive[int(menuActive)][int(subMenuActive)]))
     elif(subMenuActive != 0):
         subMenu_Selection = int(input(": "))#int(input(menuListActive[int(menuActive)][int(subMenuActive)]))
-         if(subMenu_Selection == int(input(menuListActive[int(menuActive)][int(subMenuActive)]))+1):
+        if(subMenu_Selection == int(input(menuListActive[int(menuActive)][int(subMenuActive)]))+1):
             subMenuActive = 0
+    if(str(menuListActive[int(menuActive)]  [int(postModsSub)]    [len(menuListActive [int(menuActive)]   [int(postModsSub)]    )-1]  [  subMenuActive*  ]) == "O"):
+      print("Yetettetette")
